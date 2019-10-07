6d834dd7777c9ff019a040370366ac3980494169
Dusan Matejka <matejkadusan32@gmail.com>
2019-10-07 15:51:25 +0200

    * DefaultDependecies=no caused service file to run asap usually before local-fs.target
    * Added additional checks & cleanups to bash script

--- firstboot-provision.sh
+++ firstboot-provision.sh
@@ -8,6 +8,12 @@ SCRIPT_LOG=/var/log/provision-error.log
 SCRIPT_DIR=/usr/share/vmcli/
 SERVICE_FILE=firstboot-provision.service
 
+function log_error {
+    MSG=$1
+    echo "$(date) $MSG" >> $SCRIPT_LOG
+    return 0
+}
+
 # Gather information about OS distribution and first interface used
 DISTRIBUTION=$(egrep '^ID=' /etc/os-release | sed 's/"//g' | awk -F'=' '{print $2}'| tr '[:upper:]' '[:lower:]')
 INTERFACE=$(ip link | egrep '^[1-9]:' | grep -v lo: | awk '{print $2}' | sed 's/://')
@@ -27,16 +33,17 @@ if [[ $DISTRIBUTION == 'redhat' || $DISTRIBUTION == 'centos' ]]
     NETWORK_SERVICE=network.service
 fi
 
+# Verify that OS family was properly identified
 if [[ -z $OSFAMILY || -z $INTERFACES_DEST ]]
   then
-    echo "Unable to parse OS family" >> $SCRIPT_LOG
+    log_error "Unable to parse OS family from /etc/os-release"
     exit 1
 fi
 
-# Ensure vmtoolsd binary is present
-if [[ $(whereis vmtoolsd | awk '{print $2}') == "" ]]
+# Check if vmtoolsd binary is present
+if [[ ! -x /usr/bin/vmtoolsd ]]
  then
-    echo "No vmtoolsd found on system" >> $SCRIPT_LOG
+    log_error "No vmtoolsd found installed on the provisioned system"
     exit 1
 fi
 
@@ -49,9 +56,10 @@ if [[ $PROVISION_ENABLED == 'true' ]]
     ADDRESS=$(vmtoolsd --cmd "info-get guestinfo.provision.network.address" 2>&1)   
     GATEWAY=$(vmtoolsd --cmd "info-get guestinfo.provision.network.gateway" 2>&1)
 
-    if ! echo $ADDRESS | egrep -qE $IPREGEXP || ! echo $GATEWAY | egrep -qE $IPREGEXP
+    # Verify that parsed IP addresses from extra config are valid
+    if [[ ! $ADDRESS =~ $IPREGEXP || ! $GATEWAY =~ $IPREGEXP ]]
       then
-        echo "Unable to parse IP addr or gateway from VM extra config" >> $SCRIPT_LOG
+        log_error "Unable to parse IP addr or gateway from VM's extra config"
         exit 3
     fi
 
@@ -79,15 +87,19 @@ if [[ $PROVISION_ENABLED == 'true' ]]
     systemctl start $NETWORK_SERVICE
 
     # Clean up after self & self
-    systemctl disable $SERVICE_FILE
+    systemctl disable --quiet $SERVICE_FILE
     rm /etc/systemd/system/$SERVICE_FILE
     rm -r $SCRIPT_DIR
     systemctl daemon-reload
 
-    # Clean old logs and history
-    > /root/.bash_history
+    # Cleanup system (old logs, history, udev rules, tmp files, etc)
+    rm /var/log/*.gz /var/log/dmesg.old ~root/.bash_history
+    unset HISTFILE
+    find /var/log/ -type f -exec sh -c '>"{}"' \;
+    > /etc/machine-id
+    rm -f /etc/udev/rules.d/70*
 
 else
-    # Provision is not explicitly allowed in VM extra config
-    exit 2
+    log_error "Provision is not explicitly allowed in VM extra config. Exitting..."
+    exit 0
 fi
